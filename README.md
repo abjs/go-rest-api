4ff2dedc8fca8108ca9b690ba207515a4a6a8f3b
Abin Joseph <itsmeabjs@gmail.com>
2023-06-29 04:01:33 +0530

    feat: db init

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - Setup an common business logic for commend for users in internal/commend/commend.go
 - Setup Dockerfile and docker-compose files
 - Go task cli setup
+- Setup an db connection in internal/db/db.go
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	database "github.com/abjs/go-rest-api/internal/db"
+)
 
 func Run() error {
-	fmt.Printf("App starting....")
+	fmt.Println("app starting....")
+
+	db, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the db..")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected to database")
+
 	return nil
 
 }
